a1533bd0aa1af0f91e32803514ccc34fc66a47a0
TejaChanti <raviteja75@outlook.com>
2024-03-11 15:49:27 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Product from './components/Product';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Product />
     <App />
-    
   </React.StrictMode>
 );
 
